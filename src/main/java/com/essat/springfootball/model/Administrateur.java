431cc202e784185d7777b4afb9fab681299ca018
hazem <hazem.benhassen@isimg.tn>
2024-12-30 14:59:39 +0100

    Initial commit for Spring Boot backend

---
+++ src/main/java/com/essat/springfootball/model/Administrateur.java
@@ -0,0 +1,22 @@
+package com.essat.springfootball.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Administrateurs")
+public class Administrateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nom;
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/essat/springfootball/model/Reservation.java
@@ -0,0 +1,28 @@
+package com.essat.springfootball.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String date_res;
+    private int heure_debut;
+    private int heure_fin;
+    @ManyToOne
+    private Adherant user;
+    @ManyToOne
+    @JoinColumn(name = "terrain_id")
+    private Terrain terrain;
+
+
+}
